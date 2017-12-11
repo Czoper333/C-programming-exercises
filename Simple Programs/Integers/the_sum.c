fe124b90a20b6278037077074095d8b33e177f85
x.oper <leszek.czuba@gmail.com>
2017-12-11 19:37:53 +0100

    the_sum.c

---
+++ Simple Programs/Integers/the_sum.c
@@ -0,0 +1,23 @@
+/* This program check whether the number is odd or even
+   This program is written by x.oper and this code is available
+   on the git repository at https://github.com/Czoper333 */
+
+//include standard input output library
+#include <stdio.h>
+
+int main(void)
+{
+    // declare variables
+    int a, b, sum;
+
+    // prompt user for two numbers to add
+    printf("Enter the first number to add: ");
+    scanf("%d", &a);
+    printf("Enter the second number to add: ");
+    scanf("%d", &b);
+    sum = a + b;
+
+    // print the result of addiction
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    return 0;
+}
