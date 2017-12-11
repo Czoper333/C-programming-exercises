53fa12a46f8ab59500d1b099f8eaa76617dd862c
x.oper <leszek.czuba@gmail.com>
2017-12-11 18:38:33 +0100

    half_pyramid.c

--- half_pyramid.c
+++ half_pyramid.c
@@ -7,17 +7,17 @@
 int main(void)
 {
     //declare variables
-    int i, j, rows;
+    int rows, i, j;
 
     //prompt user for the number of rows of the pyramid
-    printf("Enter number of rows \nto print half pyramid: ");
-    scanf("%d",&rows);
+    printf("Enter the number of rows \nto print half pyramid: ");
+    scanf("%d", &rows);
 
     //print rows of the pyramid
-    for(i=1; i<=rows; ++i)
+    for(i = 1; i <= rows; ++i)
     {
         //print #
-        for(j=1; j<=i; ++j)
+        for(j = 1; j <= i; ++j)
         {
             printf("# ");
         }
