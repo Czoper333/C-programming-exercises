43e01b87413b1285ddf769a67143b5c2ab4fda72
x.oper <leszek.czuba@gmail.com>
2017-12-12 12:29:55 +0100

    binary_decimal.c

---
+++ Simple Programs/Number Conversion/binary_decimal.c
@@ -0,0 +1,29 @@
+/* This program convert binary into decimals
+   This program is written by x.oper and this code is available
+   on the git repository at https://github.com/Czoper333 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // declare variables
+    int num, binary_val, decimal_val = 0, base = 1, rem;
+
+    // prompt the user for input binary
+    printf("Enter a binary number91s and 0s) \n");
+    scanf("%d", &num);
+    binary_val = num;
+
+    // conversion of a binary number to a decimal number
+    while (num > 0)
+    {
+        rem = num % 10;
+        decimal_val = decimal_val + rem * base;
+        num = num / 10;
+        base = base *2;
+    }
+
+    // print results
+    printf("The binary number is = %d \n", binary_val);
+    printf("Its decimal equivalent is = %d \n", decimal_val);
+}
