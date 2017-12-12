3aa49fd874a1333b3f064a7366b61df7fcc81c29
x.oper <leszek.czuba@gmail.com>
2017-12-12 13:36:57 +0100

    days_weeks.c

---
+++ Simple Programs/Number Conversion/days_weeks.c
@@ -0,0 +1,28 @@
+/* This program convert a given number of days in terms of Years, Weeks & Days
+   This program is written by x.oper and this code is available
+   on the git repository at https://github.com/Czoper333 */
+
+// declare constant variable
+#define DAYSINWEEK 7
+
+#include <stdio.h>
+
+int main(void)
+{
+    // declare variables
+    int ndays, year, week, days;
+
+    // prompt the user for input
+    printf("Enter the number of days\n");
+    scanf("%d", &ndays);
+
+    // equation
+    year = ndays / 365;
+    week = (ndays % 365) / DAYSINWEEK;
+    days = (ndays % 365) % DAYSINWEEK;
+
+    // print the result
+    printf("%d is equivalent to %d years, %d weeks and %d days", ndays, year, week, days);
+
+    return 0;
+}
