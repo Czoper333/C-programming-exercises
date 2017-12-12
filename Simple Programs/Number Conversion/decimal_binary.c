922f440b176026560c194b3fa5921a68532149f5
x.oper <leszek.czuba@gmail.com>
2017-12-12 12:49:01 +0100

    decimal_binary.c

---
+++ Simple Programs/Number Conversion/decimal_binary.c
@@ -0,0 +1,37 @@
+/* This program convert binary into decimals and count the number of 1s
+   This program is written by x.oper and this code is available
+   on the git repository at https://github.com/Czoper333 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // declare variables
+    long num, decimal_num, remainder, base = 1, binary = 0, no_of_1s = 0;
+
+    // prompt the user for the input
+    printf("Enter a decimal integer: \n");
+    scanf("%ld", &num);
+    decimal_num = num;
+
+    // conversion decimal to binary
+    while (num > 0)
+    {
+        remainder = num % 2;
+
+        if (remainder == 1)
+        {
+            no_of_1s++;
+        }
+        binary = binary + remainder * base;
+        num = num / 2;
+        base = base * 10;
+    }
+
+    // print the results
+    printf("Input number is = %d\n", decimal_num);
+    printf("Its binary equivalent is = %ld\n", binary);
+    printf("No.of 1's in the binary number is = %d\n", no_of_1s);
+
+    return 0;
+}
