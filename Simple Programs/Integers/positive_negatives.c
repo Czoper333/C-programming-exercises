8ebd386770afdaaae92ac6670ef5d03c7279e54d
leszekadam <leszek.czuba@gmail.com>
2018-06-22 08:16:21 +0200

    Update positive_negatives.c

--- Simple Programs/Integers/positive_negatives.c
+++ Simple Programs/Integers/positive_negatives.c
@@ -1,6 +1,4 @@
-/* This program check whether the number is positive or negative
-   This program is written by x.oper and this code is available
-   on the git repository at https://github.com/Czoper333 */
+/* This program check whether the number is positive or negative */
 
 //include standard input output library
 #include <stdio.h>
