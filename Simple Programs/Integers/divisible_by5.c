bdea300244accedd48d640fb936dd960dfb4b587
leszekadam <leszek.czuba@gmail.com>
2018-06-22 08:15:09 +0200

    Update divisible_by5.c

--- Simple Programs/Integers/divisible_by5.c
+++ Simple Programs/Integers/divisible_by5.c
@@ -1,6 +1,4 @@
-/* This program check whether the number is divisible by 5
-   This program is written by x.oper and this code is available
-   on the git repository at https://github.com/Czoper333 */
+/* This program check whether the number is divisible by 5 */
 
 // include standard input output library
 #include <stdio.h>
